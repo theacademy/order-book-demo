e666b0445ef9522f9485bcea213e500fa444c2d3
Kishore Pashindla <kishore.pashindla@gmail.com>
2026-03-09 11:11:25 +0000

    Change in GetMapping

--- src/main/java/com/mthree/orderbookdemo/controllers/HomeController.java
+++ src/main/java/com/mthree/orderbookdemo/controllers/HomeController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
 	
 	@GetMapping("/")
-	public String home() {
+	public String hello() {
 		return "Welcome to Spring Boot Application!!";
 	}
 	
