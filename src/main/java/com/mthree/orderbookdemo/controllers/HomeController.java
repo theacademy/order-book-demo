2ca281ad31fd8f5d1c3e221417b0aebad287fd1b
Kishore Pashindla <kishore.pashindla@gmail.com>
2026-06-05 09:03:03 +0100

    HomeController changed

--- src/main/java/com/mthree/orderbookdemo/controllers/HomeController.java
+++ src/main/java/com/mthree/orderbookdemo/controllers/HomeController.java
@@ -8,18 +8,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1")
 public class HomeController {
 	
-	@GetMapping("/")
-	public String hello() {
 
-		try {
-			System.out.println("Testing!!!");
-		}
-		catch(Exception e){
+@GetMapping("/")
+public String hello() {
 
-		}
-		return "Welcome to Spring Boot Application!!";
+	try {
+		System.out.println("Testing!!!");
 	}
-	
+	catch(Exception e){
+		// Exception handling - currently suppressed
+	}
+	return "Welcome to Spring Boot Application!!";
+}
 	
 
 }
